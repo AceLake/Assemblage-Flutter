00e0dc8bfbcf3813d59ec47fe2553649c4e36f71
Ace Lake <96988100+AceLake@users.noreply.github.com>
2023-10-26 15:08:40 -0700

    Got Firebase to be connected to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:messaging_app/firebase_options.dart';
+import 'package:messaging_app/services/auth/auth_gate.dart';
 
 import 'services/auth/login_or_register.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -13,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthGate(),
     );
   }
 }
---
+++ lib/services/auth/auth_gate.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:messaging_app/pages/login_page.dart';
+import 'package:messaging_app/services/auth/login_or_register.dart';
+
+import '../../pages/home_page.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // If the user is logged in
+        if (snapshot.hasData) {
+          return const HomePage();
+        } else {
+          return const LoginOrRegister();
+        }
+        // if the user isn't logged in
+      },
+    ));
+  }
+}
