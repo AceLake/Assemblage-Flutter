e49920d1cc2586fe2b37d14c90325b3da218f42c
Ace Lake <96988100+AceLake@users.noreply.github.com>
2023-11-01 16:35:26 -0700

    add a new document for the user in users collection

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthService extends ChangeNotifier {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // instance of firestore
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<UserCredential> signInWithEmailandPassword(
       String email, String password) async {
@@ -13,17 +17,30 @@ class AuthService extends ChangeNotifier {
         password: password,
       );
 
+      // add a new document for the user in users collection if it doesn't already exist
+      _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      }, SetOptions(merge: true));
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
     }
   }
 
+  // Creating a new user
   Future<UserCredential> signUpWithEmailandPassword(
       String email, password) async {
     try {
       UserCredential userCredential = await _firebaseAuth
           .createUserWithEmailAndPassword(email: email, password: password);
+
+      // after creating the user, create a document for the user in the users collection
+      _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      });
 
       return userCredential;
     } on FirebaseAuthException catch (e) {
