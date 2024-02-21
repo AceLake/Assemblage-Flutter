b061d7855d5003a620239a73b326fefd55ac1332
Ace Lake <96988100+AceLake@users.noreply.github.com>
2024-02-21 14:49:26 -0700

    messaging

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,6 +1,10 @@
+import 'dart:html';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:messaging_app/components/my_text_field.dart';
 import 'package:messaging_app/services/chat/chat_service.dart';
 
 class ChatPage extends StatefulWidget {
@@ -35,27 +39,69 @@ class _ChatPageState extends State<ChatPage> {
       appBar: AppBar(title: Text(widget.receiverUserEmail)),
       body: Column(
         children: [
-          Expanded(child: _builMessageList(),
+          Expanded(
+            child: _buildMessageList(),
           ),
-
           _buildMessageInput(),
         ],
       ),
     );
   }
 
+  Widget _buildMessageList() {
+    return StreamBuilder(
+        stream: _chatService.getMessages(
+            widget.receiverUserID, _firebaseAuth.currentUser!.uid),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error${snapshot.error}');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Loading..');
+          }
+          return ListView(
+            children: snapshot.data!.docs
+                .map((document) => _buildMessageItem(document))
+                .toList(),
+          );
+        });
+  }
+
   Widget _buildMessageInput() {
     return Row(
       children: [
-        
-    ],),
+        Expanded(
+          child: MyTextField(
+            controller: _messageController,
+            hintText: 'Enter message',
+            obscureText: false,
+          ),
+        ),
+        IconButton(
+          onPressed: sendMessage,
+          icon: const Icon(
+            Icons.arrow_upward,
+            size: 40,
+          ),
+        ),
+      ],
+    );
   }
 
-    Widget _builMessageList() {
-    return Row(
-      children: [
-        
-    ],),
-  }
+  Widget _buildMessageItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data() as Map<String, dynamic>;
 
+    var alignment = (data['senderId'] == _firebaseAuth.currentUser!.uid)
+        ? Alignment.centerRight
+        : Alignment.centerLeft;
+
+    return Container(
+        alignment: alignment,
+        child: Column(
+          children: [
+            Text(data['senderEmail']),
+            Text(data['message']),
+          ],
+        ));
+  }
 }
